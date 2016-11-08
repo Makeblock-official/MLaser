3261e3422ee093cfcf2997cdeb61a1ad1b499a00
Makeblock <info@makeblock.cc>
2016-11-08 11:49:14 +0800

    fix MAC hexdownload

--- src/UICompment/hexloadbase.cpp
+++ src/UICompment/hexloadbase.cpp
@@ -1,4 +1,5 @@
 #include "hexloadbase.h"
+#include "qdebug.h"
 
 HexLoadBase::HexLoadBase(QObject *parent) :
     QObject(parent)
@@ -10,11 +11,17 @@ void HexLoadBase::HexLoad(QString filename ,QString a)
 {
     QStringList po;
     QString arg = tr("-Uflash:w:%1:i").arg(filename);
-    QString comName = "-P"+a;
     //UNO para
     //po<<"-C./avrdude.conf"<<"-v"<<"-v"<<"-v"<<"-v"<<"-patmega328p"<<"-carduino"<<comName<<"-b115200"<<"-D"<<arg;
+#if define Q_OS_DARWIN || define Q_OS_DARWIN64
+    QString comName = "-P"+a;
     po<<"-C./avrdude.conf"<<"-v"<<"-v"<<"-v"<<"-v"<<"-patmega2560"<<"-cwiring"<<comName<<"-b115200"<<"-D"<<arg;
-    download->start("./avrdude.exe",po);
+    download->start("./avrdude.exe",po );
+#else
+    QString comName = "-P/dev/cu."+a;
+    po<<"-C./avrdude.linux.conf"<<"-v"<<"-v"<<"-v"<<"-v"<<"-patmega2560"<<"-cwiring"<<comName<<"-b115200"<<"-D"<<arg;
+    download->start("./avrdude",po );
+#endif
 //    qDebug()<<po;
     download->waitForFinished();
     emit Sig_FinishHexLoad();
